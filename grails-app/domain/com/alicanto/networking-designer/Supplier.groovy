71f238a07a4a405d29b1e1a29fb5b3c6b95e8b4c
Arnold Paye <arnoldpayet@gmail.com>
2015-04-19 15:19:05 -0400

    add supplier domain class

---
+++ grails-app/domain/com/alicanto/networking-designer/Supplier.groovy
@@ -0,0 +1,17 @@
+package com.alicanto.networking-designer
+
+class Supplier {
+
+    String name
+    String description
+    String address
+    Boolean status
+
+    User createdBy
+    Date createdDate
+    User updatedBy
+    Date updatedDate
+
+    static constraints = {
+    }
+}
